74072f15f7ab49b52bc1573c8ffcef040eeaada7
Krzysztof Antonowski <krzysztofantonowski@proton.me>
2024-02-29 13:30:28 +0100

    dobra chyba działa

--- app/components/DatePicker.tsx
+++ app/components/DatePicker.tsx
@@ -16,22 +16,20 @@ export default function DatePicker({
       />
       Data wylotu
       <input
-        type="datetime-local"
+        type="date"
         onChange={(event) => {
           onDepartureDate(new Date(event.target.value));
         }}
-        max="275760-09-12T23:59:59"
         min={new Date().toISOString().split('T')[0]}
         className="dark:bg-slate-800 bg-slate-200 dark:text-slate-100 text-slate-900 mx-2 rounded-md px-2 py-0.5 w-1/6"
       />
       <FaPlaneArrival className="ml-10 mr-2" />
       Data przylotu
       <input
-        type="datetime-local"
+        type="date"
         onChange={(event) => {
           onArrivalDate(new Date(event.target.value));
         }}
-        max="275760-09-12T23:59:59"
         min={new Date().toISOString().split('T')[0]}
         className="dark:bg-slate-800 bg-slate-200 dark:text-slate-100 text-slate-900 mx-2 rounded-md px-2 py-0.5 w-1/6"
       />
