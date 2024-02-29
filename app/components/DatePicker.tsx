40b625721e48f451579844fd127021f139e1549b
Krzysztof Antonowski <krzysztofantonowski@proton.me>
2024-02-29 13:04:54 +0100

    no kurwa mać nie działa

--- app/components/DatePicker.tsx
+++ app/components/DatePicker.tsx
@@ -21,7 +21,7 @@ export default function DatePicker({
           onDepartureDate(new Date(event.target.value));
         }}
         max="275760-09-12T23:59:59"
-        min={new Date()}
+        min={new Date().toISOString()}
         className="dark:bg-slate-800 bg-slate-200 dark:text-slate-100 text-slate-900 mx-2 rounded-md px-2 py-0.5 w-1/6"
       />
       <FaPlaneArrival className="ml-10 mr-2" />
@@ -32,7 +32,7 @@ export default function DatePicker({
           onArrivalDate(new Date(event.target.value));
         }}
         max="275760-09-12T23:59:59"
-        min={new Date()}
+        min={new Date().toISOString()}
         className="dark:bg-slate-800 bg-slate-200 dark:text-slate-100 text-slate-900 mx-2 rounded-md px-2 py-0.5 w-1/6"
       />
     </div>
